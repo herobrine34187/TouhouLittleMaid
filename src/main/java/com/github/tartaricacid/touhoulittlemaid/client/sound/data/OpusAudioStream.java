43befca68fc9ab69cfb59521b830e0c077b5566b
tartaric_acid <baka943@qq.com>
2025-02-22 01:26:39 +0800

    实验性内容 - 采用 Opus 音频来压缩模组体积 - 暂时移除 mp3 库

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/client/sound/data/OpusAudioStream.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/client/sound/data/OpusAudioStream.java
@@ -1,57 +1,29 @@
 package com.github.tartaricacid.touhoulittlemaid.client.sound.data;
 
-import io.github.jaredmdobson.concentus.OpusDecoder;
+import com.github.tartaricacid.touhoulittlemaid.util.OpusDecoderUtil;
 import io.github.jaredmdobson.concentus.OpusException;
 import net.minecraft.client.sounds.AudioStream;
-import org.gagravarr.ogg.OggFile;
-import org.gagravarr.opus.OpusAudioData;
-import org.gagravarr.opus.OpusFile;
+import org.apache.commons.lang3.tuple.Pair;
 import org.lwjgl.BufferUtils;
 
 import javax.sound.sampled.AudioFormat;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 
 public class OpusAudioStream implements AudioStream {
-    /**
-     * Opus 支持的帧长有：2.5ms、5ms、10ms、20ms、40ms、60ms
-     * <p>
-     * 因为我们对延迟不是很敏感，所以选 60ms
-     */
-    private static final int MAX_FRAME_SIZE = 60;
-
     private final InputStream stream;
     private final AudioFormat format;
     private final int frameSize;
     private final byte[] frame;
 
     public OpusAudioStream(byte[] data) throws IOException, OpusException {
-        // 为了方便，我们直接一口气把它全部解码了
-        try (OpusFile opusFile = new OpusFile(new OggFile(new ByteArrayInputStream(data)));
-             ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-            int sampleRate = opusFile.getInfo().getSampleRate();
-            int channels = opusFile.getInfo().getNumChannels();
-            int frameSize = sampleRate * MAX_FRAME_SIZE / 1000;
-
-            OpusDecoder decoder = new OpusDecoder(sampleRate, channels);
-            byte[] pcmBytes = new byte[sampleRate * channels * 2];
-            OpusAudioData packet;
-
-            while ((packet = opusFile.getNextAudioPacket()) != null) {
-                byte[] packetBytes = packet.getData();
-                int packetLength = packetBytes.length;
-                int samplesDecoded = decoder.decode(packetBytes, 0, packetLength, pcmBytes, 0, frameSize, false);
-                outputStream.write(pcmBytes, 0, samplesDecoded * channels * 2);
-            }
-            this.stream = new ByteArrayInputStream(outputStream.toByteArray());
-            this.format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, sampleRate, 16,
-                    channels, channels * 2, sampleRate, false);
-            this.frameSize = this.format.getFrameSize();
-            this.frame = new byte[this.frameSize];
-        }
+        Pair<AudioFormat, byte[]> decoded = OpusDecoderUtil.decode(data);
+        this.stream = new ByteArrayInputStream(decoded.getRight());
+        this.format = decoded.getLeft();
+        this.frameSize = this.format.getFrameSize();
+        this.frame = new byte[this.frameSize];
     }
 
     @Override
