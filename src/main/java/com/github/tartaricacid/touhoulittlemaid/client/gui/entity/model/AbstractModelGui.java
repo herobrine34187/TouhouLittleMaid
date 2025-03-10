5e605cdd821677c4675ec3c11d34d54e77cd2b81
tartaric_acid <baka943@qq.com>
2025-03-11 00:15:13 +0800

    部分修正 - 模型切换界面文本提示添加 model id 和模型包名 - 现在女仆提示可以通过配置逐个关闭了 - 修正坐船骑乘扫帚时，文本提示总是闪烁的问题

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/client/gui/entity/model/AbstractModelGui.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/client/gui/entity/model/AbstractModelGui.java
@@ -20,10 +20,7 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.texture.AbstractTexture;
 import net.minecraft.client.renderer.texture.SimpleTexture;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.FormattedText;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
@@ -467,8 +464,13 @@ public abstract class AbstractModelGui<T extends LivingEntity, E extends IModelI
                 // 塞入提示语
                 if (!modelItem.getName().equals(ENCRYPT_EGG_NAME) && !modelItem.getName().equals(NORMAL_EGG_NAME)) {
                     tooltips.add(Component.translatable("gui.touhou_little_maid.skin.tooltips.show_details")
-                            .withStyle(ChatFormatting.DARK_GRAY, ChatFormatting.ITALIC));
+                            .withStyle(ChatFormatting.DARK_PURPLE));
                 }
+                // 当开启显示更多物品信息功能时，显示模型 ID
+                if (getMinecraft().options.advancedItemTooltips) {
+                    tooltips.add(Component.literal(modelItem.getModelId().toString()).withStyle(ChatFormatting.DARK_GRAY));
+                }
+                tooltips.add(ParseI18n.parse(pack.getPackName()).withStyle(ChatFormatting.BLUE));
                 // 绘制解析过的文本提示
                 graphics.renderComponentTooltip(font, tooltips, mouseX, mouseY);
             }
