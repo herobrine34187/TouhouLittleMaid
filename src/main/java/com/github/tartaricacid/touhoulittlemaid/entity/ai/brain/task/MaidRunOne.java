88d8a54b999ec5577be2f8b8d3a8bc200feca7f3
tartaric_acid <baka943@qq.com>
2025-03-11 14:26:59 +0800

    部分内容修正 - enableLookAndRandomWalk 方法现在可以随时关闭启用随机乱走 - 换回旧版音频，提升音频质量 - 修改刷怪蛋材质 - 新增 crys 材质

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/entity/ai/brain/task/MaidRunOne.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/entity/ai/brain/task/MaidRunOne.java
@@ -8,14 +8,25 @@ import net.minecraft.world.entity.ai.behavior.BehaviorControl;
 import net.minecraft.world.entity.ai.behavior.RunOne;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class MaidRunOne extends RunOne<EntityMaid> {
-    public MaidRunOne(List<Pair<? extends BehaviorControl<? super EntityMaid>, Integer>> pEntryCondition) {
-        super(pEntryCondition);
+    private final Function<EntityMaid, Boolean> enableCondition;
+
+    public MaidRunOne(List<Pair<? extends BehaviorControl<? super EntityMaid>, Integer>> entryCondition, Function<EntityMaid, Boolean> enableCondition) {
+        super(entryCondition);
+        this.enableCondition = enableCondition;
+    }
+
+    public MaidRunOne(List<Pair<? extends BehaviorControl<? super EntityMaid>, Integer>> entryCondition) {
+        this(entryCondition, maid -> true);
     }
 
     @Override
     public boolean tryStart(ServerLevel pLevel, EntityMaid maid, long pGameTime) {
+        if (!enableCondition.apply(maid)) {
+            return false;
+        }
         return !maid.isBegging() && !maid.isSleeping() && !(maid.getVehicle() instanceof EntitySit) && super.tryStart(pLevel, maid, pGameTime);
     }
 }
