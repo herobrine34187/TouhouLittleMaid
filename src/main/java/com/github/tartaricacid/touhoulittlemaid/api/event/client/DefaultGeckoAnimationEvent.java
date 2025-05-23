78884e7f3ed71580e20d78252fb7164fc84c3951
tartaric_acid <baka943@qq.com>
2025-05-23 21:45:23 +0800

    添加事件，能够让其他模组支持添加额外的 GeckoLib 动画 - 添加主人转换工具的材质

---
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/api/event/client/DefaultGeckoAnimationEvent.java
@@ -0,0 +1,50 @@
+package com.github.tartaricacid.touhoulittlemaid.api.event.client;
+
+import com.github.tartaricacid.touhoulittlemaid.TouhouLittleMaid;
+import com.github.tartaricacid.touhoulittlemaid.geckolib3.file.AnimationFile;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.eventbus.api.Event;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static com.github.tartaricacid.touhoulittlemaid.client.resource.GeckoModelLoader.mergeAnimationFile;
+
+/**
+ * 在客户端加载额外的默认 Gecko 动画文件。
+ */
+@OnlyIn(Dist.CLIENT)
+public class DefaultGeckoAnimationEvent extends Event {
+    private final AnimationFile maidAnimationFile;
+    private final AnimationFile tacAnimationFile;
+    private final AnimationFile chairAnimationFile;
+
+    public DefaultGeckoAnimationEvent(AnimationFile maidAnimationFile, AnimationFile tacAnimationFile, AnimationFile chairAnimationFile) {
+        this.maidAnimationFile = maidAnimationFile;
+        this.tacAnimationFile = tacAnimationFile;
+        this.chairAnimationFile = chairAnimationFile;
+    }
+
+    public AnimationFile getMaidAnimationFile() {
+        return maidAnimationFile;
+    }
+
+    public AnimationFile getTacAnimationFile() {
+        return tacAnimationFile;
+    }
+
+    public AnimationFile getChairAnimationFile() {
+        return chairAnimationFile;
+    }
+
+    public void addAnimation(AnimationFile animationFile, ResourceLocation file) {
+        try (InputStream stream = Minecraft.getInstance().getResourceManager().open(file)) {
+            mergeAnimationFile(stream, animationFile);
+        } catch (IOException e) {
+            TouhouLittleMaid.LOGGER.error("Failed to load animation file", e);
+        }
+    }
+}
