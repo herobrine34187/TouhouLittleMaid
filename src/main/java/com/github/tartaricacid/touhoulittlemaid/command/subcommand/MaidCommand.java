442315f042d8382f19a4a583eb86b01eaff72b72
tartaric_acid <baka943@qq.com>
2025-08-11 00:21:05 +0800

    现在安装了经验泵和储罐的背包可以提取女仆的经验

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/command/subcommand/MaidCommand.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/command/subcommand/MaidCommand.java
@@ -3,6 +3,7 @@ package com.github.tartaricacid.touhoulittlemaid.command.subcommand;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
@@ -17,15 +18,25 @@ import net.minecraft.world.entity.Entity;
 public class MaidCommand {
     private static final String MAID_NAME = "maid";
     private static final String TARGETS_NAME = "targets";
+
     private static final String NO_AI_NAME = "no_ai";
+    private static final String XP = "xp";
+
     private static final String RESULT_NAME = "result";
 
     public static LiteralArgumentBuilder<CommandSourceStack> get() {
         LiteralArgumentBuilder<CommandSourceStack> pack = Commands.literal(MAID_NAME);
         RequiredArgumentBuilder<CommandSourceStack, EntitySelector> targets = Commands.argument(TARGETS_NAME, EntityArgument.entity());
+
         LiteralArgumentBuilder<CommandSourceStack> noAi = Commands.literal(NO_AI_NAME);
-        RequiredArgumentBuilder<CommandSourceStack, Boolean> result = Commands.argument(RESULT_NAME, BoolArgumentType.bool());
-        pack.then(targets.then(noAi.then(result.executes(MaidCommand::handleMaidNoAi))));
+        LiteralArgumentBuilder<CommandSourceStack> xp = Commands.literal(XP);
+
+        RequiredArgumentBuilder<CommandSourceStack, Boolean> booleanResult = Commands.argument(RESULT_NAME, BoolArgumentType.bool());
+        RequiredArgumentBuilder<CommandSourceStack, Integer> intResult = Commands.argument(RESULT_NAME, IntegerArgumentType.integer());
+
+        pack.then(targets.then(noAi.then(booleanResult.executes(MaidCommand::handleMaidNoAi))));
+        pack.then(targets.then(xp.then(intResult.executes(MaidCommand::handleMaidXp))));
+
         return pack;
     }
 
@@ -36,7 +47,19 @@ public class MaidCommand {
             maid.setNoAi(noAi);
             context.getSource().sendSuccess(() -> Component.translatable("commands.touhou_little_maid.maid.no_ai.success", String.valueOf(noAi)), true);
         } else {
-            context.getSource().sendFailure(Component.translatable("commands.touhou_little_maid.maid.no_ai.fail"));
+            context.getSource().sendFailure(Component.translatable("commands.touhou_little_maid.maid.not_maid"));
+        }
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static int handleMaidXp(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+        Entity entity = EntityArgument.getEntity(context, TARGETS_NAME);
+        int xpCount = IntegerArgumentType.getInteger(context, RESULT_NAME);
+        if (entity instanceof EntityMaid maid) {
+            maid.setExperience(xpCount);
+            context.getSource().sendSuccess(() -> Component.translatable("commands.touhou_little_maid.maid.xp.success", String.valueOf(xpCount)), true);
+        } else {
+            context.getSource().sendFailure(Component.translatable("commands.touhou_little_maid.maid.not_maid"));
         }
         return Command.SINGLE_SUCCESS;
     }
