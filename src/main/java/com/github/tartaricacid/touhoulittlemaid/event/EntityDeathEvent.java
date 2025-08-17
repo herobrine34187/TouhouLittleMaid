c7b9b2fb398fe6bfc66f9c1e9287ba606f07036f
tartaric_acid <baka943@qq.com>
2025-08-17 23:42:01 +0800

    修正 DamageSource 为 null 导致的崩溃问题

--- src/main/java/com/github/tartaricacid/touhoulittlemaid/event/EntityDeathEvent.java
+++ src/main/java/com/github/tartaricacid/touhoulittlemaid/event/EntityDeathEvent.java
@@ -1,6 +1,7 @@
 package com.github.tartaricacid.touhoulittlemaid.event;
 
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -10,7 +11,11 @@ import net.minecraftforge.fml.common.Mod;
 public class EntityDeathEvent {
     @SubscribeEvent
     public static void onEntityDeath(LivingDeathEvent event) {
-        Entity causingEntity = event.getSource().getEntity();
+        DamageSource source = event.getSource();
+        if (source == null) {
+            return;
+        }
+        Entity causingEntity = source.getEntity();
         if (causingEntity instanceof EntityMaid maid) {
             maid.getKillRecordManager().onTargetDeath(maid, event.getEntity());
         }
